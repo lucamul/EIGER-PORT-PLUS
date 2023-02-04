46fa40e1fc8994ad25c0eecdef7983262e408532
lucamul <lcmultazzu@gmail.com>
2023-02-04 11:14:02 +0100

    improved copy id

--- copy_id.py
+++ copy_id.py
@@ -2,12 +2,21 @@ import os
 import string
 
 PLUS = True
-
+SETUP = False
+CMD = "\"ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCxvL9qE53SpT829WEMZgU6BGtSu7DmC8ju0ugF5vX4wL7EuaT97IlcOtR4rMCUoW/fNmfIJ+u1DZmgX5s7ZtzRVjLdazEF3my3Xk+FL8oJVu3LN7CeV986jJmyfANx3Kdb0W40wBBZ+nMtP0kTNar5+rwJ53y2Mdf3BiHSkwOqKdUVIdFkX4BU0pWO0uRpAxqQAphCBWuaxTfGOeJDu1db3zrJp6T+cLou4nK6S9IM55IzSBxFo4+NARHvxq2NAKRLtldoDPbZqrZnsr5nLY6CE8mASYjLvFQDhbwdYo0vhqNryByKr0rf+qa9ht6myKBuH3/5Nc8qfTKn2WE2uF3j luca_mul@node0.luca-mul-146592.pora.emulab.net\""
 def setup(hosts):
-    #for host in hosts:
-    #     username, domain = host.split("@")
-    #     cmd = "ssh-keygen -f \"/home/luca/.ssh/known_hosts\" -R \"" +  domain + "\""
-    #     os.system(cmd)
+    
+    if SETUP:
+        for host in hosts:
+            
+            username, domain = host.split("@")
+            cmd = "ssh-keygen -f \"/home/luca/.ssh/known_hosts\" -R \"" +  domain + "\""
+            os.system(cmd)
+            if host != hosts[0]:
+                cmd = " \"mkdir -p ~/.ssh && echo " + CMD + " >> ~/.ssh/authorized_keys && chmod 700 ~/.ssh && chmod 600 ~/.ssh/authorized_keys\""
+                os.system("ssh -o StrictHostKeyChecking=no " + host + cmd)
+        return
+    
     cmd = " \" sudo usermod -s /bin/bash luca_mul ; cd /local ; sudo rm -r ./* ; git clone https://github.com/princeton-sns/Eiger-PORT.git ; mv /local/EIGER_PORT/* /local ; sudo rm -r EIGER_PORT ; sed -i 's/node-/node/g' /local/Eiger-PORT/eval-scripts/vicci_dcl_config/16_clients_in_kodiak /local/Eiger-PORT/eval-scripts/vicci_dcl_config/16_in_kodiak ; cd Eiger-PORT ; ./install-dependencies.bash ; cd Eiger-PORT ; ant ; ant ; ant ; ant; cd tools/stress ; ant ; cd ../../../eiger ; ant ; ant ; ant  ; ant ; cd tools/stress ; ant\""
     if PLUS: 
         cmd = " \" sudo usermod -s /bin/bash luca_mul ; cd /local ; sudo rm -r ./* ; git clone https://github.com/lucamul/EIGER_PORT.git ; mv /local/EIGER_PORT/* /local ; sudo rm -r EIGER_PORT ; sed -i 's/node-/node/g' /local/Eiger-PORT/eval-scripts/vicci_dcl_config/16_clients_in_kodiak /local/Eiger-PORT/eval-scripts/vicci_dcl_config/16_in_kodiak ; cd Eiger-PORT ; ./install-dependencies.bash ; cd Eiger-PORT ; ant ; ant ; ant ; ant; cd tools/stress ; ant ; cd ../../../eiger ; ant ; ant ; ant  ; ant ; cd tools/stress ; ant\""
@@ -18,39 +27,39 @@ def setup(hosts):
 
 
 hosts_port_plus = [
-"luca_mul@pc504.emulab.net",		
-"luca_mul@pc518.emulab.net",		
-"luca_mul@pc507.emulab.net",		
-"luca_mul@pc488.emulab.net",		
-"luca_mul@pc482.emulab.net",		
-"luca_mul@pc480.emulab.net",		
-"luca_mul@pc536.emulab.net",		
-"luca_mul@pc549.emulab.net",		
-"luca_mul@pc431.emulab.net",		
-"luca_mul@pc545.emulab.net",		
-"luca_mul@pc473.emulab.net",		
-"luca_mul@pc557.emulab.net",		
-"luca_mul@pc436.emulab.net",		
-"luca_mul@pc516.emulab.net",		
-"luca_mul@pc538.emulab.net",		
-"luca_mul@pc521.emulab.net",		
-"luca_mul@pc542.emulab.net",		
-"luca_mul@pc500.emulab.net",		
-"luca_mul@pc541.emulab.net",		
-"luca_mul@pc487.emulab.net",		
-"luca_mul@pc476.emulab.net",		
-"luca_mul@pc512.emulab.net",		
-"luca_mul@pc553.emulab.net",		
-"luca_mul@pc534.emulab.net",		
-"luca_mul@pc522.emulab.net",		
-"luca_mul@pc508.emulab.net",		
-"luca_mul@pc550.emulab.net",		
-"luca_mul@pc529.emulab.net",		
-"luca_mul@pc552.emulab.net",		
-"luca_mul@pc420.emulab.net",		
-"luca_mul@pc503.emulab.net",		
-"luca_mul@pc427.emulab.net",		
-"luca_mul@pc535.emulab.net"	
+"luca_mul@pc419.emulab.net", 		
+"luca_mul@pc552.emulab.net", 		
+"luca_mul@pc549.emulab.net", 		
+"luca_mul@pc434.emulab.net", 		
+"luca_mul@pc461.emulab.net", 		
+"luca_mul@pc436.emulab.net", 		
+"luca_mul@pc538.emulab.net", 		
+"luca_mul@pc475.emulab.net", 		
+"luca_mul@pc458.emulab.net", 		
+"luca_mul@pc405.emulab.net", 		
+"luca_mul@pc459.emulab.net", 		
+"luca_mul@pc522.emulab.net", 		
+"luca_mul@pc431.emulab.net", 		
+"luca_mul@pc462.emulab.net", 		
+"luca_mul@pc550.emulab.net", 		
+"luca_mul@pc420.emulab.net", 		
+"luca_mul@pc422.emulab.net", 		
+"luca_mul@pc471.emulab.net", 		
+"luca_mul@pc411.emulab.net", 		
+"luca_mul@pc432.emulab.net", 		
+"luca_mul@pc540.emulab.net", 		
+"luca_mul@pc414.emulab.net", 		
+"luca_mul@pc450.emulab.net", 		
+"luca_mul@pc466.emulab.net", 		
+"luca_mul@pc547.emulab.net", 		
+"luca_mul@pc467.emulab.net", 		
+"luca_mul@pc557.emulab.net", 		
+"luca_mul@pc449.emulab.net", 		
+"luca_mul@pc439.emulab.net", 		
+"luca_mul@pc469.emulab.net", 		
+"luca_mul@pc427.emulab.net", 		
+"luca_mul@pc442.emulab.net", 		
+"luca_mul@pc468.emulab.net" 		
 ]
 
 hosts_port = [
@@ -89,4 +98,4 @@ hosts_port = [
 "luca_mul@pc425.emulab.net"
 ]
 
-setup(hosts_port)
+setup(hosts_port_plus)
